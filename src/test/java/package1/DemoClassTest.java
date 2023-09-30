d62218decf136a4dc24cfc8b41bc5d44709fa063
Prasad-Jodu <prasadjodu12@gmail.com>
2023-09-30 16:27:47 +0530

    Practice commit

---
+++ src/test/java/package1/DemoClassTest.java
@@ -0,0 +1,11 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class DemoClassTest {
+	@Test
+	public void demoClassTest() {
+		System.out.println("Demo class is created");
+	}
+
+}
